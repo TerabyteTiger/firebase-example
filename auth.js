da5584083b6f8d738748a858b4fead59bbf98073
12vanblart <12vanblart@gmail.com>
2019-08-06 12:59:55 -0400

    🎉

--- auth.js
+++ auth.js
@@ -2,18 +2,18 @@ const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 const uiConfig = {
   callbacks: {
-		signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-			return true;
-		},
-		uiShown: function() {
-			document.getElementById("loader").style.display = "none";
-		},
-	},
-  signInFlow: "popup",
-  signInSuccessUrl: "signedIn",
+    signInSuccessWithAuthResult(authResult, redirectUrl) {
+      return true;
+    },
+    uiShown() {
+      document.getElementById('loader').style.display = 'none';
+    },
+  },
+  signInFlow: 'popup',
+  signInSuccessUrl: 'signedIn',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID
-  ]
-}
-ui.start('#firebaseui-auth-container', uiConfig)
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+};
+ui.start('#firebaseui-auth-container', uiConfig);
